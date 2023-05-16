c61ddd60fcef26736ff2e4e860235b2f463a4654
Hugo1506 <aluno24171@ipt.pt>
2023-05-16 12:50:14 +0100

    alguns atributos do comprador

--- Models/Comprador.cs
+++ Models/Comprador.cs
@@ -2,5 +2,19 @@
 {
     public class Comprador
     {
+        public string login { get; set; }
+
+        public string password { get; set; }
+
+        public string name { get; set; }
+
+        public string telefone { get; set; }
+
+        public string email { get; set; }
+
+        public int dinheiro { get; set; }
+
+
+
     }
 }
